046a56d5ddad4b11b4e856019a81821272f09978
ruzrobert <nufriaj@gmail.com>
2022-07-29 21:36:42 +0300

    * Project upload

---
+++ OpenSeaMetadataHelper/MetadataTypes.cs
@@ -0,0 +1,251 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace OpenSeaMetadataHelper
+{
+	/// <summary>
+	/// https://docs.opensea.io/docs/metadata-standards
+	/// </summary>
+	public class Metadata
+	{
+		/// <summary>
+		/// Name of the item.
+		/// </summary>
+		[JsonProperty("name")]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// A human readable description of the item. Markdown is supported.
+		/// </summary>
+		[JsonProperty("description")]
+		public string Description { get; set; }
+
+		/// <summary>
+		/// This is the URL to the image of the item. Can be just about any type of image (including SVGs, which will be cached into PNGs by OpenSea), and can be IPFS URLs or paths. We recommend using a 350 x 350 image.
+		/// </summary>
+		[JsonProperty("image")]
+		public string Image { get; set; }
+
+		/// <summary>
+		/// Raw SVG image data, if you want to generate images on the fly (not recommended). Only use this if you're not including the image parameter.
+		/// </summary>
+		[JsonProperty("image_data")]
+		public string ImageData { get; set; }
+
+		/// <summary>
+		/// This is the URL that will appear below the asset's image on OpenSea and will allow users to leave OpenSea and view the item on your site.
+		/// </summary>
+		[JsonProperty("external_url")]
+		public string ExternalUrl { get; set; }
+
+		/// <summary>
+		/// Background color of the item on OpenSea. Must be a six-character hexadouble without a pre-pended #.
+		/// </summary>
+		[JsonProperty("background_color")]
+		public string BackgroundColor { get; set; }
+
+		/// <summary>
+		/// A URL to a multi-media attachment for the item. The file extensions GLTF, GLB, WEBM, MP4, M4V, OGV, and OGG are supported, along with the audio-only extensions MP3, WAV, and OGA.
+		/// 
+		/// Animation_url also supports HTML pages, allowing you to build rich experiences and interactive NFTs using JavaScript canvas, WebGL, and more.Scripts and relative paths within the HTML page are now supported. However, access to browser extensions is not supported.
+		/// </summary>
+		[JsonProperty("animation_url")]
+		public string AnimationUrl { get; set; }
+
+		/// <summary>
+		/// A URL to a YouTube video.
+		/// </summary>
+		[JsonProperty("youtube_url")]
+		public string YoutubeUrl { get; set; }
+
+		/// <summary>
+		/// These are the attributes for the item, which will show up on the OpenSea page for the item. (see below)
+		/// </summary>
+		[JsonProperty("attributes")]
+		public List<MdAttribute> Attributes { get; set; }
+		
+		public void Save(string path)
+		{
+			string json = Serialize();
+			Directory.CreateDirectory(Path.GetDirectoryName(path));
+			File.WriteAllText(path, json, Encoding.UTF8);
+		}
+
+		public string Serialize()
+		{
+			OnBeforeSerializing();
+
+			JsonSerializerSettings settings = new JsonSerializerSettings
+			{
+				NullValueHandling = NullValueHandling.Ignore,
+				Converters = new[] { new DoubleJsonConverter() }
+			};
+			return JsonConvert.SerializeObject(this, Formatting.Indented, settings);
+		}
+
+		private void OnBeforeSerializing()
+		{
+			if (ImageData != null) Image = null;
+			Require(BackgroundColor == null || (!BackgroundColor.StartsWith("#") && BackgroundColor.Length == 6));
+		}
+
+		private static void Require(bool check)
+		{
+			if (!check) throw new InvalidOperationException();
+		}
+	}
+
+	public class MdAttribute
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonProperty("display_type")]
+		public string DisplayType { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonProperty("trait_type")]
+		public string TraitType { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonProperty("value", NullValueHandling = NullValueHandling.Include)]
+		public object Value { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonProperty("max_value")]
+		public object MaxValue { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[JsonIgnore]
+		public DateTime DateValue
+		{
+			set
+			{
+				DisplayType = SerializableDisplayTypes.FromDisplayType(OpenSeaMetadataHelper.DisplayType.Date);
+				Value = ((DateTimeOffset)value).ToUnixTimeSeconds();
+			}
+		}
+
+		public MdAttribute(DisplayType displayType, string traitType, string value) : this(displayType, traitType, value, null) { }
+		public MdAttribute(DisplayType displayType, string traitType, double value) : this(displayType, traitType, value, null) { }
+		public MdAttribute(DisplayType displayType, string traitType, double value, double maxValue)
+											: this(displayType, traitType, (object)value, maxValue) { }
+
+		private MdAttribute(DisplayType displayType, string traitType, object value, double? maxValue)
+		{
+			DisplayType = SerializableDisplayTypes.FromDisplayType(displayType);
+			TraitType = traitType;
+			Value = value;
+			MaxValue = maxValue.HasValue ? (object)maxValue.Value : null;
+		}
+
+		public MdAttribute(string traitType, DateTime date)
+		{
+			TraitType = traitType;
+			DateValue = date;
+		}
+
+		public MdAttribute(string traitType, string value)
+		{
+			TraitType = traitType;
+			Value = value;
+		}
+
+		public MdAttribute(string traitType, double value)
+		{
+			TraitType = traitType;
+			Value = value;
+		}
+
+		public MdAttribute(string traitType, double value, double maxValue)
+		{
+			TraitType = traitType;
+			Value = value;
+			MaxValue = maxValue;
+		}
+
+		public MdAttribute(string value) => Value = value;
+		public MdAttribute(double value) => Value = value;
+
+		private static class SerializableDisplayTypes
+		{
+			public const string None = null;
+			public const string Number = "number";
+			public const string BoostPercentage = "boost_percentage";
+			public const string BoostNumber = "boost_number";
+			public const string Date = "date";
+
+			public static string[] GetValues() => new[] { None, Number, BoostPercentage, BoostNumber, Date };
+
+			public static string FromDisplayType(DisplayType displayType)
+			{
+				return GetValues()[(int)displayType];
+			}
+		}
+	}
+
+	public enum DisplayType
+	{
+		None = 0,
+		Number = 1,
+		BoostPercentage = 2,
+		BoostNumber = 3,
+		Date = 4,
+	}
+
+	/// <summary>
+	/// https://stackoverflow.com/questions/21153381/json-net-serializing-float-double-with-minimal-decimal-places-i-e-no-redundant
+	/// https://stackoverflow.com/questions/65093530/prevent-newtonsoft-json-from-adding-trailing-0
+	/// </summary>
+	public class DoubleJsonConverter : JsonConverter<double>
+	{
+		public override bool CanRead => false;
+
+		public override double ReadJson(JsonReader reader, Type objectType, double existingValue, bool hasExistingValue, JsonSerializer serializer)
+		{
+			throw new NotImplementedException("Unnecessary because CanRead is false. The type will skip the converter.");
+		}
+
+		public override void WriteJson(JsonWriter writer, double value, JsonSerializer serializer)
+		{
+			if (IsWholeValue(value))
+			{
+				writer.WriteRawValue(JsonConvert.ToString(Convert.ToInt64(value)));
+			}
+			else
+			{
+				writer.WriteRawValue(JsonConvert.ToString(value));
+			}
+		}
+
+		private static bool IsWholeValue(object value)
+		{
+			if (value is decimal decimalValue)
+			{
+				int precision = (decimal.GetBits(decimalValue)[3] >> 16) & 0x000000FF;
+				return precision == 0;
+			}
+			else if (value is float floatValue)
+			{
+				return floatValue == Math.Truncate(floatValue);
+			}
+			else if (value is double doubleValue)
+			{
+				return doubleValue == Math.Truncate(doubleValue);
+			}
+
+			return false;
+		}
+	}
+}
---
+++ OpenSeaMetadataHelper/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using static System.Environment;
+
+namespace OpenSeaMetadataHelper
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("OpenSea Metadata Helper");
+			Console.WriteLine();
+
+			RunHelper();
+
+			Console.WriteLine("Done!!!");
+			Console.ReadKey();
+		}
+
+		private static void RunHelper()
+		{
+			// Examples
+
+			Metadata metadata = new Metadata()
+			{
+				Name = "Dave Starbelly",
+				Description = "Friendly OpenSea Creature that enjoys long swims in the ocean.",
+				Image = "https://storage.googleapis.com/opensea-prod.appspot.com/puffs/3.png",
+				ExternalUrl = "https://openseacreatures.io/3",
+				Attributes = new List<MdAttribute>()
+				{
+					new MdAttribute("Base", "Starfish"),
+					new MdAttribute("Eyes", "Big"),
+					new MdAttribute("Mouth", "Surprised"),
+					new MdAttribute("Level", 5),
+					new MdAttribute("Stamina", 1.4),
+					new MdAttribute("Personality", "Sad"),
+					new MdAttribute(DisplayType.BoostNumber, "Aqua Power", 40),
+					new MdAttribute(DisplayType.BoostPercentage, "Stamina Increase", 10),
+					new MdAttribute(DisplayType.Number, "Generation", 2),
+					new MdAttribute(DisplayType.Date, "Birthday1", 1546360800),
+					new MdAttribute("Birthday2", new DateTime(2019, 1, 1, 18, 40, 0)),
+					new MdAttribute("Happy"),
+					new MdAttribute("MaxValued", 10, 100),
+				},
+			};
+
+			//Metadata metadata = new Metadata()
+			//{
+			//	Name = "#1",
+			//	Description = "The Meta Colonialists Season 1! 1 of 500 NFTs.",
+			//	Image = "https://gateway.pinata.cloud/ipfs/QmSttbAufQ9o9x6KS1YNregWdXJCAYxjHgaZD2GavR7uBs/1.png",
+			//	ExternalUrl = "https://metacolonialists.io/",
+			//	Attributes = new List<MdAttribute>()
+			//	{
+			//		new MdAttribute("Background", "Black_background"),
+			//		new MdAttribute("Birthday", new DateTime(2012, 2, 23)),
+			//		new MdAttribute("Mystic"),
+			//		new MdAttribute("In Development"),
+			//		new MdAttribute("In Development", 12, 200),
+			//	},
+			//};
+
+			metadata.Save(Path.Combine(GetFolderPath(SpecialFolder.DesktopDirectory), "metadata_0.json"));
+		}
+	}
+}
